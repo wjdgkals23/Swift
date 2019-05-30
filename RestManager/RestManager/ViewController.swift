ffa4bc386bc927afc5826581051ff08b5c7610d1
wjdgkals23 <wjdgkals74@gmail.com>
2019-05-31 06:19:47 +0900

    [학습] 테스트 셋팅, 리퀘스트 생성방식 학습

--- RestManager/RestManager/ViewController.swift
+++ RestManager/RestManager/ViewController.swift
@@ -9,10 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var restManager: RestManager = RestManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getUser()
     }
     
+    func getUser() {
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        restManager.makeRequest(toURL: url, withHttpMethod: .get) { (results) in
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
+                print(userData.description)
+            }
+            
+            print("\n\nResponse HTTP Headers:\n")
+            
+            if let response = results.response {
+                for (key, value) in response.headers.allValues() {
+                    print(key, value)
+                }
+            }
+        }
+    }
+    
+    func createUser() {
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        restManager.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+        restManager.httpBodyParameters.add(value: "John", forKey: "name")
+        restManager.httpBodyParameters.add(value: "Developer", forKey: "job")
+        
+        restManager.makeRequest(toURL: url, withHttpMethod: .post) { (results) in
+            guard let response = results.response else { return }
+            if response.httpStatusCode == 201 {
+                guard let data = results.data else { return }
+                let decoder = JSONDecoder()
+                guard let jobUser = try? decoder.decode(JobUser.self, from: data) else { return }
+                print(jobUser.description)
+            }
+        }
+    }
 }
---
+++ RestManager/RestManagerTests/RestManagerTests.swift
@@ -0,0 +1,33 @@
+//
+//  RestManagerTests.swift
+//  RestManagerTests
+//
+//  Created by 정하민 on 31/05/2019.
+//  Copyright © 2019 Appcoda. All rights reserved.
+//
+
+import XCTest
+
+class RestManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
