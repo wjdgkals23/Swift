2b8b038762e1e5eea0019b780a1e7c441579e8e9
wjdgkals23 <wjdgkals74@gmail.com>
2019-05-30 16:17:47 +0900

    [학습] Closure Swtich Enum Class/Structure비교

---
+++ playground/Closure.playground/Contents.swift
@@ -0,0 +1,105 @@
+import UIKit
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+} // 전역함수 : 이름이 있고 어떤 값도 캡쳐하지 않는 클로저
+
+names.sorted(by: backward) // 절대 names를 바꾸는 것 ㄴㄴ!
+var sortedNames = names.sorted(by: { (s1:String, s2:String) -> Bool in
+    return s1 > s2
+})
+var sortedNames2 = names.sorted() { return $0 > $1 }
+sortedNames
+sortedNames2
+
+// 후위 클로저
+
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+let numbers = [16, 58, 510]
+
+let strings = numbers.map { (number) -> String in
+    var number = number
+    print(number)
+    var output = ""
+    
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    
+    return output
+}
+
+print(strings)
+
+// 중첩 함수 _ 값 캡쳐
+func makeIncrementer(forIncrement amount: Int) -> () -> Int { // Int 인자를 받고 () -> Int 형태의 함수를 반환한다.
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+
+incrementByTen()
+incrementByTen()
+incrementByTen()
+incrementByTen()
+
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+incrementBySeven()
+incrementBySeven()
+incrementBySeven()
+
+
+// 이스케이핑 클로저 : 큐를 이용한 비동기 연산의 완료 핸들러, 밖에서 처리될 함수는 필수!!
+var completionHandlers: [() -> Void] = []
+func someFunctionWithEscapingClosure(completionHandler: @escaping () -> Void) {
+    completionHandlers.append(completionHandler)
+}
+
+func someFunctionWithNonescapingClosure(closure: () -> Void) {
+    closure()    // 함수 안에서 끝나는 클로저
+}
+
+class SomeClass {
+    var x = 10
+    func doSomething() {
+        someFunctionWithEscapingClosure { [weak self] in self?.x = 100 } // 외부에서 처리될 함수에 대해 weak self를 처리하는 이유는 어떠한 이유던지 해당 Class를 크래시로 부터 보존하기 위함이다.
+        someFunctionWithNonescapingClosure { x = 200 }
+    }
+}
+
+let instance = SomeClass()
+instance.doSomething()
+print(instance.x)
+
+completionHandlers.first?()
+print(instance.x)
+
+var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+print(customersInLine.count)
+// Prints "5"
+
+let customerProvider = { customersInLine.remove(at: 0) }
+print(customersInLine.count)
+// Prints "5"
+
+print("Now serving \(customerProvider())!")
+// Prints "Now serving Chris!"
+print(customersInLine.count)
+// Prints "4"
+
+func serve(customer customerProvider: @autoclosure () -> String) {
+    print("Now serving \(customerProvider())!")
+}
+serve(customer: { customersInLine.remove(at: 0) } )
---
+++ playground/Switch.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+let approximateCount = 100
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+
+enum Str: String {
+    case Test, Case, Temp, Each
+}
+
+let temp:Str = Str.Case
+
+switch temp {
+case .Case:
+    print("here")
+default:
+    print("not")
+}
+
+//재귀열거자?
+enum ArithmeticExpression {
+    case number(Int)
+    indirect case addition(ArithmeticExpression, ArithmeticExpression) // 재귀형태
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+print(evaluate(product))
